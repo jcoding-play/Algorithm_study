f4bd7eb50312176fbdd6bd0d05fb6d24f5ccc0be
조경찬 (Jo Gyungchan) <namejgc@naver.com>
2022-12-03 10:54:57 +0900

    [ADD] - 11720 java
    숫자의 합 구하기

---
+++ baekjoon/string/B_11720.java
@@ -0,0 +1,22 @@
+package baekjoon_1.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_11720 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		
+		int sum = 0;
+		for(int i=0; i<n; i++) {
+			sum += str.charAt(i)-'0';
+		}
+		
+		System.out.print(sum);
+	}
+
+}
