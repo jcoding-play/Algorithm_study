62df0c8a6bae6d4ee996e441c720184593b7d5b5
조경찬 (Jo Gyungchan) <namejgc@naver.com>
2022-12-05 11:01:55 +0900

    [UPDATE] - 11720 java 클래스명 변경

--- baekjoon/string/B_11720.java
+++ baekjoon/string/B_11720.java
@@ -1,10 +1,8 @@
-package baekjoon_1.string;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B_11720 {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
