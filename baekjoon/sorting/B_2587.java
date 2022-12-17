a6fd7fd5240e98a495b0862fe1106c1a29e1770d
조경찬 (Jo Gyungchan) <namejgc@naver.com>
2022-12-18 03:34:33 +0900

    [ADD] - 2587 java

---
+++ baekjoon/sorting/B_2587.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[5];
+		int sum = 0;
+		for(int i=0; i<5; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		
+		for(int i=0; i<4; i++) {
+			for(int j=i+1; j<5; j++) {
+				if(arr[i]>arr[j]) {
+					int tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+		
+		System.out.println(sum/5);
+		System.out.println(arr[2]);
+		
+	}
+
+}
